c887c2433ebb5ca76a24d4e4b735a5b723a003fe
LiliaZheltiakova <lilia.zheltiakova@gmail.com>
2020-11-06 11:13:58 +0800

    Day 1

---
+++ DiceGame/Assets/_Scripts/Controller.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private static Controller m_instance;
+    public static Controller Instance 
+    {
+        get 
+        {
+            if(m_instance == null)
+            {
+                var ctrl = Instantiate(Resources.Load("Prefabs/Controller")) as GameObject;
+                m_instance = ctrl.GetComponent<Controller>();
+            }
+
+            return m_instance;
+        }
+    }
+
+    [SerializeField] private int m_fieldSize;
+    public int FieldSize 
+    {
+        get { return m_fieldSize; }
+        set { m_fieldSize = value; }
+    }
+    [SerializeField] private int m_emptySquares;
+    [SerializeField] private int m_tokenTypes;
+    public int TokenTypes 
+    {
+        get { return m_tokenTypes; }
+        set { m_tokenTypes = value; }
+    }
+    [SerializeField] private Color[] m_tokenColors;
+    public Color[] TokenColors 
+    {
+        get { return m_tokenColors; }
+        set { m_tokenColors = value; }
+    }
+
+    private List<List<Token>> m_tokensByTypes;
+
+    public List<List<Token>> TokensByTypes 
+    {
+        get { return m_tokensByTypes; }
+        set { m_tokensByTypes = value; }
+    }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(m_instance == null)
+        {
+            m_instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        else
+        {
+            if(m_instance != this)
+                Destroy(this.gameObject);
+        }
+
+        m_tokenColors = MakeColor(m_tokenTypes);
+
+        TokensByTypes = new List<List<Token>>();
+
+        for(int i = 0; i < TokenTypes; i++)
+        {
+            TokensByTypes.Add(new List<Token>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Color[] MakeColor(int count)
+    {
+        Color[] result = new Color[count];
+        float colorStep = 1f / (count + 1);
+        float hue = 0f;
+        float saturation = .5f;
+        float value = 1f;
+
+        for(int i = 0; i < count; i++)
+        {
+            float newHue = hue + (colorStep * i);
+
+            result[i] = Color.HSVToRGB(newHue, saturation, value);
+        }
+
+        return result;
+    }
+
+    public void TurnDone()
+    {
+        if(IsAllTokensConnected())
+        {
+            Debug.Log("Win!");
+        }
+        else
+        {
+            Debug.Log("Continue...");
+        }
+    }
+
+    public bool IsAllTokensConnected()
+    {
+        for(int i = 0; i < TokensByTypes.Count; i++)
+        {
+            if(IsTokensConnected(TokensByTypes[i]) == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private bool IsTokensConnected(List<Token> tokens)
+    {
+        if(tokens.Count == 0)
+        {
+            return true;
+        }
+
+        List<Token> connectedTokens = new List<Token>();
+        connectedTokens.Add(tokens[0]);
+
+        bool moved = true;
+
+        while(moved)
+        {
+            moved = false;
+
+            for(int i = 0; i < connectedTokens.Count; i++)
+            {
+                for(int j = 0; j < tokens.Count; j++)
+                {
+                    if(IsTokenNear(tokens[j], connectedTokens[i]))
+                    {
+                        if(connectedTokens.Contains(tokens[j]) == false)
+                        {
+                            connectedTokens.Add(tokens[j]);
+                            moved = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        if(tokens.Count == connectedTokens.Count)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool IsTokenNear(Token first, Token second)
+    {
+        if((int)first.transform.position.x == (int)second.transform.position.x + 1 || (int)first.transform.position.x == (int)second.transform.position.x - 1)
+        {
+            if((int)first.transform.position.y == (int)second.transform.position.y)
+            {
+                return true;
+            }
+        }
+
+        if((int)first.transform.position.y == (int)second.transform.position.y + 1 || (int)first.transform.position.y == (int)second.transform.position.y - 1)
+        {
+            if((int)first.transform.position.x == (int)second.transform.position.x)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ DiceGame/Assets/_Scripts/Token.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Token : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Camera m_camera;
+    private Vector3 m_pointerPositionBeforeDrag;
+    private Vector3 m_positionBeforeDrag;
+    private int[] m_dragSpace;
+    private int m_tokenType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_camera = Camera.main;
+        AlignOnGrid();
+        m_tokenType = UnityEngine.Random.Range(0, Controller.Instance.TokenTypes);
+        Material myMaterial = gameObject.GetComponent<Renderer>().material;
+        myMaterial.SetColor("_Color", Controller.Instance.TokenColors[m_tokenType]);
+        Controller.Instance.TokensByTypes[m_tokenType].Add(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        m_pointerPositionBeforeDrag = m_camera.ScreenToWorldPoint(eventData.position);
+        m_positionBeforeDrag = this.transform.position;
+
+        GetDragSpace();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 mouseWorldPosition = m_camera.ScreenToWorldPoint(eventData.position);
+        //mouse offset relative to the point where we drag the dice
+        Vector3 totalDrag = mouseWorldPosition - m_pointerPositionBeforeDrag;
+
+        if(Mathf.Abs(totalDrag.x) > Mathf.Abs(totalDrag.y)) //check for the movement direction
+        {
+            //movement in empty raws only
+            float posX = Mathf.Clamp(mouseWorldPosition.x, m_pointerPositionBeforeDrag.x - m_dragSpace[1], m_positionBeforeDrag.x + m_dragSpace[0]);
+            //horizontal movement
+            this.transform.position = new Vector3(posX, m_pointerPositionBeforeDrag.y, this.transform.position.z);
+        }
+        else
+        {
+            //movement in empty columns only
+            float posY = Mathf.Clamp(mouseWorldPosition.y, m_pointerPositionBeforeDrag.y - m_dragSpace[3], m_positionBeforeDrag.y + m_dragSpace[2]); 
+            //vertical movement
+            this.transform.position = new Vector3(m_pointerPositionBeforeDrag.x, posY, this.transform.position.z);
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        AlignOnGrid();
+        Controller.Instance.TurnDone();
+    }
+
+    private void AlignOnGrid()
+    {
+        Vector3 alignedPosition = this.transform.position;
+        alignedPosition.x = Mathf.Round(this.transform.position.x);
+        alignedPosition.y = Mathf.Round(this.transform.position.y);
+        this.transform.position = alignedPosition;
+    }
+
+    private void GetDragSpace()
+    {
+        int OddEven = 1;
+        if(Controller.Instance.FieldSize % 2 != 0)
+        {
+            OddEven = 0;
+        }
+        m_dragSpace = new int[] {0, 0, 0, 0};
+        int halfField = (Controller.Instance.FieldSize - 1) / 2;
+
+        m_dragSpace[0] = CheckSpace(Vector2.right);
+
+        if(m_dragSpace[0] == -1)
+        {
+            m_dragSpace[0] = halfField + (int)this.transform.position.x + OddEven;
+        }
+
+        m_dragSpace[1] = CheckSpace(Vector2.left);
+
+        if(m_dragSpace[1] == -1)
+        {
+            m_dragSpace[1] = halfField - (int)this.transform.position.x;
+        }
+
+        m_dragSpace[2] = CheckSpace(Vector2.up);
+
+        if(m_dragSpace[2] == -1)
+        {
+            m_dragSpace[2] = halfField - (int)this.transform.position.y + OddEven;
+        }
+
+        m_dragSpace[3] = CheckSpace(Vector2.down);
+
+        if(m_dragSpace[3] == -1)
+        {
+            m_dragSpace[3] = halfField + (int)this.transform.position.y;
+        }
+    }
+
+    private int CheckSpace(Vector2 direction)
+    {
+        RaycastHit2D[] hits = Physics2D.RaycastAll(this.transform.position, direction);
+
+        for(int i = 0; i < hits.Length; i++)
+        {
+            if(hits[i].collider.gameObject != gameObject)
+            {
+                return Mathf.FloorToInt(hits[i].distance);
+            }
+        }
+
+        return -1;
+    }
+}
